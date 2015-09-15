17e55d3a716bb3a7a71d413dd864808e6667d957
George Slight <George.slight@itlab.com>
2015-09-15 16:21:03 +0100

    First push

---
+++ bloatwaredl.bat
@@ -0,0 +1,83 @@
+@echo off
+for /f %%a in ('WMIC OS GET LocalDateTime ^| find "."') DO set DTS=%%a
+set CUR_DATE=%DTS:~0,4%-%DTS:~4,2%-%DTS:~6,2%
+
+
+cls
+color 0f
+set SCRIPT_VERSION=1.0.0
+set SCRIPT_DATE=2015-09-15
+title bloatwaredl v%SCRIPT_VERSION% (%SCRIPT_DATE%)
+
+set REPO_URL=
+set REPO_SCRIPT_DATE=0
+set REPO_SCRIPT_VERSION=0
+
+:: PREP: Update check
+:: Use wget to fetch sha256sums.txt from the repo and parse through it. Extract latest version number and release date from last line (which is always the latest release)
+wget.exe --no-check-certificate %REPO_URL%/sha256sums.txt -O %TEMP%\sha256sums.txt 2>NUL
+:: Assuming there was no error, go ahead and extract version number into REPO_SCRIPT_VERSION, and release date into REPO_SCRIPT_DATE
+if /i %ERRORLEVEL%==0 (
+	for /f "tokens=1,2,3 delims= " %%a in (%TEMP%\sha256sums.txt) do set WORKING=%%b
+	for /f "tokens=4 delims=,()" %%a in (%TEMP%\sha256sums.txt) do set WORKING2=%%a
+	)
+if /i %ERRORLEVEL%==0 (
+	set REPO_SCRIPT_VERSION=%WORKING:~1,6%
+	set REPO_SCRIPT_DATE=%WORKING2%
+	)
+
+
+:: Notify if an update was found
+SETLOCAL ENABLEDELAYEDEXPANSION
+if /i %SCRIPT_VERSION% LSS %REPO_SCRIPT_VERSION% (
+	set CHOICE=y
+	color 8a
+	cls
+	echo.
+	echo  ^^! A newer version of Tron is available on the official repo.
+	echo.
+	echo    Your version:   %SCRIPT_VERSION% ^(%SCRIPT_DATE%^)
+	echo    Latest version: %REPO_SCRIPT_VERSION% ^(%REPO_SCRIPT_DATE%^)
+	echo.
+	echo    Option 1: Sync directly from repo using BT Sync read-only key:
+	echo     %REPO_BTSYNC_KEY%
+	echo.
+	echo    Option 2: Download the latest self-extracting .exe yourself:
+	echo     %REPO_URL%
+	echo.
+	echo    Option 3: Automatically download latest .exe to the desktop
+	echo              ^(This copy of Tron will self-destruct afterwards^)
+	echo.
+	set /p CHOICE= Auto-download latest version now? [Y/n]:
+	if !CHOICE!==y (
+		color 8B
+		cls
+		echo.
+		echo %TIME%   Downloading new version to the desktop, please wait...
+		echo.
+		wget.exe --no-check-certificate "%REPO_URL%/Tron v%REPO_SCRIPT_VERSION% (%REPO_SCRIPT_DATE%).exe" -O "%USERPROFILE%\Desktop\Tron v%REPO_SCRIPT_VERSION% (%REPO_SCRIPT_DATE%).exe"
+		echo.
+		echo %TIME%   Download finished.
+		echo.
+		echo %TIME%   Verifying SHA256 pack integrity, please wait...
+		echo.
+		hashdeep.exe -s -e -b -v -a -k %TEMP%\sha256sums.txt "%USERPROFILE%\Desktop\bloatwaredl.bat" | find /i "Files matched: 1"
+		if !ERRORLEVEL!==0 (
+			echo %TIME%   SHA256 pack integrity verified. The new version is on your desktop.
+			echo.
+			popd
+			pause
+			echo. && ENDLOCAL DISABLEDELAYEDEXPANSION
+		) else (
+			color 0c
+			echo %TIME% ^^! ERROR: Download FAILED the integrity check. Recommend manually
+			echo                      downloading latest version.
+			echo.
+			pause
+			REM Clean up after ourselves
+			del /f /q %TEMP%\sha256sums.txt
+			exit
+		)
+	)
+	color 0f
+)
